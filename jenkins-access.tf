00ed81a9034114ba9da71e547a5ed2bedd877dcc
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2026-02-26 13:13:05 +0000

    use variable

--- jenkins-access.tf
+++ jenkins-access.tf
@@ -1,11 +1,6 @@
-data "azurerm_user_assigned_identity" "jenkins" {
-  name                = "jenkins-cftptl-intsvc-mi"
-  resource_group_name = "managed-identities-cftptl-intsvc-rg"
-}
-
 resource "azurerm_key_vault_access_policy" "jenkins" {
   key_vault_id = azurerm_key_vault.kv.id
-  object_id = data.azurerm_user_assigned_identity.jenkins.principal_id
+  object_id = var.jenkins_object_id
   tenant_id = data.azurerm_client_config.current.tenant_id
 
   certificate_permissions = [
