17eab2f6fbdf91b9375d60854c9ee1d5ff9ab38b
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2026-02-26 11:08:28 +0000

    add environment specific jenkins access policy

---
+++ jenkins-access.tf
@@ -0,0 +1,52 @@
+data "azurerm_user_assigned_identity" "jenkins" {
+  name                = "jenkins-${var.env}-mi"
+  resource_group_name = "managed-identities-${var.env}-rg"
+}
+
+resource "azurerm_key_vault_access_policy" "jenkins" {
+  key_vault_id = azurerm_key_vault.kv.id
+  object_id = data.azurerm_user_assigned_identity.jenkins.principal_id
+  tenant_id = data.azurerm_client_config.current.tenant_id
+
+  certificate_permissions = [
+    "Create",
+    "Delete",
+    "DeleteIssuers",
+    "Get",
+    "GetIssuers",
+    "Import",
+    "List",
+    "ListIssuers",
+    "SetIssuers",
+    "Update",
+    "ManageContacts",
+    "ManageIssuers",
+  ]
+
+  key_permissions = [
+    "Create",
+    "List",
+    "Get",
+    "Delete",
+    "Update",
+    "Import",
+    "Backup",
+    "Restore",
+    "Decrypt",
+    "Encrypt",
+    "UnwrapKey",
+    "WrapKey",
+    "Sign",
+    "Verify",
+    "GetRotationPolicy",
+  ]
+
+  secret_permissions = [
+    "Set",
+    "List",
+    "Get",
+    "Delete",
+    "Recover",
+    "Purge",
+  ]
+}
