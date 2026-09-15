e6b938b3e6bc0fd8781033979d61763c11b0e16e
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2026-09-15 10:52:40 +0100

    account for idam-vault-sandbox

--- data.tf
+++ data.tf
@@ -26,7 +26,7 @@ data "azuread_service_principal" "jenkins_dev" {
 }
 
 data "azuread_service_principal" "jenkins_ptl" {
-  display_name = var.env != "sbox" && var.env != "sandbox" ? (
+  display_name = var.env != "sbox" && var.env != "sandbox" && var.env != "idam-vault-sandbox" ? (
     local.business_area == "cft" ? "jenkins-cftptl-intsvc-mi" : "jenkins-ptl-mi"
     ) : (
     local.business_area == "cft" ? "jenkins-cftsbox-intsvc-mi" : "jenkins-ptlsbox-mi"
