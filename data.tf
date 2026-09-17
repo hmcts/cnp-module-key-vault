3c9dfd7eef9bcf8702f84cfe8c39f4f12242c846
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2026-09-17 10:20:35 +0100

    remove unused data resource

--- data.tf
+++ data.tf
@@ -1,9 +1,5 @@
 data "azurerm_client_config" "current" {}
 
-data "azuread_service_principal" "current" {
-  client_id = data.azurerm_client_config.current.client_id
-}
-
 data "azuread_group" "developers" {
   display_name = var.developers_group
 }
